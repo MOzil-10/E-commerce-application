07aa4ab153894d11b343252da383ff26014a5cc1
MOzil-10 <mukosibudeli@gmail.comm>
2024-12-31 13:49:57 +0200

    Update

--- src/main/java/com/e_commerce/app/service/ProductService.java
+++ src/main/java/com/e_commerce/app/service/ProductService.java
@@ -1,7 +1,14 @@
 package com.e_commerce.app.service;
 
+import com.e_commerce.app.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
 }
